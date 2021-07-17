22fd9afd6303f33994ed536e2b314e7bf6d3f15d
george <geor@webdi.com>
2021-07-17 20:06:41 +0300

    display button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import { useFetch } from './useFetch'
-import Follower from './Follower'
+import React, {useState, useEffect} from 'react';
+import {useFetch} from './useFetch';
+import Follower from './Follower';
 
 function App() {
   const {loading, data} = useFetch();
@@ -11,28 +11,46 @@ function App() {
   const [followers, setFollowers] = useState([]);
 
   useEffect(() => {
-    if(loading) return
-    setFollowers(data[page])
-  }, [loading])
+    if (loading) return;
+    setFollowers(data[page]);
+  }, [loading, page]);
 
- 
+  const handlePage = index => {
+    setPage(index);
+  };
 
-  return <main>
-    <div className="section-title">
-      <h1>{loading ? 'loading...' : 'pagination'}</h1>
-      <div className="underline"></div>
-    </div>
-
-    <section className="followers">
-      <div className="container">
-
-        {followers.map((follower) => {
-          return <Follower key={follower.id} {...follower} />
-        })}
-
+  return (
+    <main>
+      <div className='section-title'>
+        <h1>{loading ? 'loading...' : 'pagination'}</h1>
+        <div className='underline'></div>
       </div>
-    </section>
-  </main>
+
+      <section className='followers'>
+        <div className='container'>
+          {followers.map(follower => {
+            return <Follower key={follower.id} {...follower} />;
+          })}
+        </div>
+
+        {!loading && (
+          <div className='btn-container'>
+            {data.map((item, index) => {
+              return (
+                <button
+                  key={index}
+                  className={`page-btn ${index === page && 'active-btn'}`}
+                  onClick={() => handlePage(index)}
+                >
+                  {index + 1}
+                </button>
+              );
+            })}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
