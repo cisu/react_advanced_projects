787ffad5684e9e95794d11e9d92bc2d9aa01a2cc
george <geor@webdi.com>
2021-07-17 19:45:30 +0300

    pagination logic

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,14 @@
-const paginate = () => {}
+const paginate = (followers) => {
+    const itemsPerPage = 11;
+    const pages = Math.ceil(followers.length / itemsPerPage);
+   
+    // create new array with equal items as pages;
+    const newFollowers = Array.from({length: pages}, (_, index) => {
+        const start = index * itemsPerPage;
+        return followers.slice(start, start + itemsPerPage)
+    })
+
+    return newFollowers
+}
 
 export default paginate
