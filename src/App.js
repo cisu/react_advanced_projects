987987ffa65ab8bb9287243d9d27fbca76a368bb
george <geor@webdi.com>
2021-07-22 23:00:02 +0300

    initial fetch

--- src/App.js
+++ src/App.js
@@ -1,128 +1,41 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react'
+import { FaSearch } from 'react-icons/fa'
+import Photo from './Photo'
+// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const mainUrl = `https://api.unsplash.com/photos/`
+const searchUrl = `https://api.unsplash.com/search/photos/`
 
-import {
-  FaEnvelopeOpen,
-  FaUser,
-  FaCalendarTimes,
-  FaMap,
-  FaPhone,
-  FaLock,
-} from 'react-icons/fa';
 
-const url = 'https://randomuser.me/api/';
-const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg';
+
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [person, setPerson] = useState(null);
-  const [title, setTitle] = useState('name');
-  const [value, setValue] = useState('random person');
 
-  const getPerson = async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    const person = data.results[0];
-    const { phone, email } = person;
-    const { large : image } = person.picture;
-    const { login : {password} } = person;
-    const { first, last } = person.name;
-    const { dob: {age} } =person;
-    const { street: {number, name} } = person.location;
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState(['']);
 
-    const newPerson = {
-      image, 
-      phone, 
-      email, 
-      password, 
-      age, 
-      street: `${number} ${name}`,
-      name: `${first} ${last}`
+
+  const fetchImages = async () =>{
+    setLoading(true)
+
+    let url;
+    url = `${mainUrl}?client_id=JYL4cDyvfSzsnLp2hcRkf_SWotv7_dxo60gz47gZ0fk`
+
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
     }
-
-    setPerson(newPerson);
-    setLoading(false);
-    setTitle('name');
-    setValue(newPerson.name);
   }
 
-  useEffect(() =>{
-    getPerson();
-  })
+  useEffect(() => {
+    fetchImages();
+  }, [])
 
-  const handleValue = e => {
-    if(e.target.classList.contains('icon')){
-      const newValue = e.target.dataset.label;
-      // console.log(person);
-      setTitle(newValue);
-      // console.log(newValue);
-      setValue(person[newValue]);
-    }
-  };
 
-  return (
-    <main>
-      <div className='block bcg-black'></div>
-      <div className='block'>
-        <div className='container'>
-          <img
-            src={(person && person.image) || defaultImage}
-            alt='random user'
-            className='user-img'
-          />
-          <p className='user-title'>my {title} is</p>
-          <p className='user-value'>{value}</p>
-          <div className='values-list'>
-            <button
-              className='icon'
-              data-label='name'
-              onMouseOver={handleValue}
-            >
-              <FaUser />
-            </button>
-            <button
-              className='icon'
-              data-label='email'
-              onMouseOver={handleValue}
-            >
-              <FaEnvelopeOpen />
-            </button>
-            <button
-              className='icon'
-              data-label='age'
-              onMouseOver={handleValue}
-            >
-              <FaCalendarTimes />
-            </button>
-            <button
-              className='icon'
-              data-label='street'
-              onMouseOver={handleValue}
-            >
-              <FaMap />
-            </button>
-            <button
-              className='icon'
-              data-label='phone'
-              onMouseOver={handleValue}
-            >
-              <FaPhone />
-            </button>
-            <button
-              className='icon'
-              data-label='password'
-              onMouseOver={handleValue}
-            >
-              <FaLock />
-            </button>
-          </div>
-
-          <button className='btn' type='button'>
-            {loading ? 'loading...' : 'random user'}
-          </button>
-        </div>
-      </div>
-    </main>
-  );
+  return <h2>stock photos starter</h2>
 }
 
-export default App;
+export default App
