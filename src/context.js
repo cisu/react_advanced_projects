cc126adef13fead338aa8fc5ef392b0ceaff224e
george <geor@webdi.com>
2021-08-01 12:01:25 +0300

    movies component

--- src/context.js
+++ src/context.js
@@ -7,44 +7,41 @@ export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT
 const AppContext = React.createContext();
 
 const AppProvider = ({children}) => {
-
   const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState({show:false, msg: ''});
+  const [error, setError] = useState({show: false, msg: ''});
   const [movies, setMovies] = useState([]);
-  const [query, setQuery] = useState('batman');
+  const [query, setQuery] = useState('sup');
 
-  const fetchMovies = async (url) => {
-    setIsLoading(true);
+  const fetchMovies = async url => {
+    setIsLoading(true);  
     try {
-      const response = await fetch(url)
+      const response = await fetch(url);
       const data = await response.json();
-
-      // check if response is true 
-      if(data.Response === 'True') {
-        setMovies(data.Search)
-        setError({show: false, msg: ''})
-      }
-      else {
-        setError({show:true,msg: data.Error})
+      // check if response is true
+      if (data.Response === 'True') {
+        setMovies(data.Search);
+        setError({show: false, msg: ''});
+      } else {
+        setError({show: true, msg: data.Error});
       }
 
       setIsLoading(false);
-      
-      console.log(data)
-    }
-    catch(error) {
-      console.log(error)
-    }
-  } 
 
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    fetchMovies(`${API_ENDPOINT}&s=${query}`)
-  }, [query])
+    fetchMovies(`${API_ENDPOINT}&s=${query}`);
+  }, [query]);
 
-  return <AppContext.Provider value={isLoading, error, movies, query, setQuery}>
-    {children}
-    </AppContext.Provider>;
+  return (
+    <AppContext.Provider value={{isLoading, error, movies, query, setQuery}}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 // make sure use
 export const useGlobalContext = () => {
