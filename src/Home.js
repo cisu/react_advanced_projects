8e93e1b554c359d348bc48aea6bf9c90cd1f9874
george <geor@webdi.com>
2021-08-01 09:51:10 +0300

    react router

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Form from './SearchForm'
 import Movies from './Movies'
+
 const Home = () => {
-  return <h2>movie component</h2>
+  return <h2>Home component</h2>
 }
 
 export default Home
