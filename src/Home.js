7eca4e6fa980c3fae40a4b836d61558789380b4e
george <geor@webdi.com>
2021-08-01 10:16:10 +0300

    Initial Fetch

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,14 @@
-import React from 'react'
-import Form from './SearchForm'
-import Movies from './Movies'
+import React from 'react';
+import Form from './SearchForm';
+import Movies from './Movies';
 
 const Home = () => {
-  return <h2>Home component</h2>
-}
+  return (
+    <main>
+      <Form />
+      <Movies />
+    </main>
+  );
+};
 
-export default Home
+export default Home;
