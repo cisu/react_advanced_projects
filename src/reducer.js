8947c2e0b1499d93b47886efaf0be132f3711d27
george <geor@webdi.com>
2021-08-07 14:03:31 +0300

    search form

--- src/reducer.js
+++ src/reducer.js
@@ -10,6 +10,7 @@ const reducer = (state, action) => {
   switch (action.type) {
     case SET_LOADING:
       return {...state, isLoading: true};
+
     case SET_STORIES:
       return {
         ...state,
@@ -17,10 +18,18 @@ const reducer = (state, action) => {
         hits: action.payload.hits,
         nbPages: action.payload.nbPages,
       };
+
     case REMOVE_STORY:
-      return {...state, hits: state.hits.filter((story)=>{
-        return story.objectID !== action.payload
-      })};
+      return {
+        ...state,
+        hits: state.hits.filter(story => {
+          return story.objectID !== action.payload;
+        }),
+      };
+
+    case HANDLE_SEARCH:
+      return {...state, query:action.payload, page:0};
+
     default:
       throw new Error(`no matching "${action.type}" action type`);
   }
