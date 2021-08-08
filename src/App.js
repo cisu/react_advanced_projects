f33078d97bb84e773243d7d4570d830d7d6f4e68
george <geor@webdi.com>
2021-08-08 19:25:26 +0300

    display Question

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Loading from './Loading';
 import Modal from './Modal';
 
 function App() {
-  const {waiting, loading, question, index, correct, error} =
+  const {waiting, loading, questions, index, correct, error} =
     useGlobalContext();
 
   // if we are waiting display SetupForm component
@@ -19,7 +19,44 @@ function App() {
     return <Loading />;
   }
 
-  return <main>quiz app</main>;
+  const {question, incorrect_answers, correct_answer} = questions[index];
+
+  const answers = [...incorrect_answers, correct_answer];
+
+  // console.log(questions[1])
+
+  return (
+    <main>
+      {/* <Modal /> */}
+
+      <section className='quiz'>
+        <p className='correct-answers'>
+          correct answers : {correct}/{index}
+        </p>
+
+        <article className='container'>
+          <h2 dangerouslySetInnerHTML={{__html: question}} />
+
+          <div className='container'>
+            {answers.map((answer, index) => {
+              return (
+                <button
+                  key={index}
+                  className='answer-btn'
+                  dangerouslySetInnerHTML={{__html: answer}}
+                />
+              );
+            })}
+          </div>
+        </article>
+
+          <button className="next-question">
+            next question
+          </button>
+
+      </section>
+    </main>
+  );
 }
 
 export default App;
